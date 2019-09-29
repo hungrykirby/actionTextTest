c42e0af6f447ad7f1e60297ffc3439142e0594c3
hungrykirby <katsuki8hanawa6@gmail.com>
2019-09-29 18:14:38 +0900

    admin user

---
+++ db/migrate/20190929085249_add_index_admins_email.rb
@@ -0,0 +1,4 @@
+class AddIndexAdminsEmail < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_29_020303) do
+ActiveRecord::Schema.define(version: 2019_09_29_085721) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -41,6 +41,14 @@ ActiveRecord::Schema.define(version: 2019_09_29_020303) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "admins", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   create_table "blogs", force: :cascade do |t|
